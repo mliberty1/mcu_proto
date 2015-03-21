45df73a9db65526d67256225e9055a8bc7af5471
Matt Liberty <matt.liberty@jetperch.com>
2015-03-21 15:14:32 -0400

    Added CC3200 RGB LED control.

--- cc3200_energia/Fade/Fade.ino
+++ cc3200_energia/Fade/Fade.ino
@@ -1,14 +1,21 @@
 /* WebSocket Client for CC3200 LaunchPad */
 
+// https://www.olimex.com/Products/Modules/LED/MOD-LED8x8RGB/open-source-hardware
+// https://github.com/OLIMEX/DUINO/tree/master/AVR/MOD-LED8x8RGB
+
 #include "wifi_config.h"
 #include <WiFi.h>
 #include "WebClient.h"
+#include <SPI.h>
+#include "font.h"
+#include "lcd8x8rgb.h"
 #define LED RED_LED
 
 char websocket_server[] = "mcu_proto.jetperch.com";
 int websocket_port = 80;
 char websocket_path[] = "/ws";
 
+boolean draw_en = true;
 WiFiClient client;
 WebsocketClient webSocketClient(websocket_server, websocket_port, websocket_path, false, wscMessage);
 
@@ -17,6 +24,7 @@ void wscMessage(char* msg)
   Serial.print("Got msg : ");
   Serial.println(msg);
   digitalWrite(LED, !digitalRead(LED));
+  draw_en = !draw_en;
 }
 
 void wifi_connect() {
@@ -59,16 +67,40 @@ void wifi_connect() {
 void setup() {
   Serial.begin(115200);
   pinMode(LED, OUTPUT);
+  
+  // Configure SPI
+  SPI.begin(); // start the SPI library
+  pinMode(chipSelectPin, OUTPUT);
+  SPI.setDataMode(SPI_MODE1);
+  SPI.setClockDivider(SPI_CLOCK_DIV16);
+
+  color = 1;
+  drawRectangle(4, 4, 5, 5);
+  Transfer();
+  vClear();
+  
   // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
   WiFi.begin(wifi_ssid, wifi_password);
   wifi_connect();
   webSocketClient.connect();
 }
 
+int i = 0;
+
 void loop() {
   String data;
   wifi_connect();
   webSocketClient.run();
+  if (draw_en) {
+    lScroll();
+    drawLine(8, 1, 8, 8);
+  }
+  ++color;
+  Transfer();
   delay(50);
+  ++i;
+  if (i >= 8) {
+    i = 0;
+  }
 }
 
