6b1efe925e9f2c97b0561b12949bfc79cf992311
Matt Liberty <matt.liberty@jetperch.com>
2015-03-21 11:45:21 -0400

    Modified WiFi config notes.

--- cc3200_energia/Fade/wifi_config.h
+++ cc3200_energia/Fade/wifi_config.h
@@ -2,6 +2,11 @@
  *
  * This file should be modified to match your network configuration.
  * (not really a .h file)
+ *
+ * To update the repository:
+ *    git update-index --no-skip-worktree cc3200_energia/Fade/wifi_config.h
+ *    Make change
+ *    git update-index --skip-worktree cc3200_energia/Fade/wifi_config.h
  */
 
 // your network name (also called SSID)
